775bc9d1b3e83b3405d5fef0e23f2c99a3352215
Fausto Rendon <contact@faustorendon.com>
2020-12-31 16:28:34 -0800

    Adds additional filter options to business search endpoint

--- src/server/services/yelp/business-search.ts
+++ src/server/services/yelp/business-search.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import axios from 'axios';
 import config from '../../../config';
+import { IGetSearchBusinessRequest } from './interfaces';
+
+const SEARCH_LIMIT = 10;
 
 const getBusinessSearch = async (req: express.Request, res: express.Response): Promise<express.Response<never>> => {
-	const { term, location } = req.query;
+	const { term, location, offset, openNow, price, sortBy }: IGetSearchBusinessRequest = req.query as never;
 	if (!location) {
 		return res.status(400).json({ error: 'Location is required.' });
 	}
-	const searchResult = await axios.get(`${config.yelp.uri}/businesses/search?term=${term}&location=${location}`, {
+	console.log('test');
+	let searchQuery = `location=${location}&limit=${SEARCH_LIMIT}`;
+	// TODO: Refactor into a helper function
+	searchQuery += term ? `&term=${term}` : '';
+	searchQuery += offset ? `&offset=${offset}` : '';
+	searchQuery += openNow ? `&open_now=${openNow}` : '';
+	searchQuery += price ? `&price=${price}` : '';
+	searchQuery += sortBy ? `&sort_by=${sortBy}` : '';
+	const searchResult = await axios.get(`${config.yelp.uri}/businesses/search?${searchQuery}`, {
 		headers: { Authorization: `Bearer ${config.yelp.key}` },
 	});
 	return res.status(200).json(searchResult.data);
---
+++ src/server/services/yelp/interfaces.ts
@@ -0,0 +1,8 @@
+export interface IGetSearchBusinessRequest {
+	location: string;
+	term: string;
+	offset: string;
+	openNow: boolean;
+	price: string;
+	sortBy: string;
+}
